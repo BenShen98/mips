661fcea1e70be4a5ba0052fa2cca9ea96c7188c2
alanyuan97 <alanyuan97@gmail.com>
2018-11-22 22:50:35 +0000

    fixed sltiu_cc.s bug issues

---
+++ src/testcase/slti_cc1.s
@@ -0,0 +1,4 @@
+#slti_cc1,1,,,testing immediate sign extend
+li $3,0xFFFFFFFE #-2
+slti $2,$3,0xFFFF
+jr $0
---
+++ src/testcase/sltiu_cc.s
@@ -0,0 +1,4 @@
+#sltiu_cc,1,,,testing immediate sign extend
+li $3,0xFFFFFFFE #-2
+sltiu $2,$3,0xFFFF
+jr $0
