149676552fd5f072d8cb0efea1b3ba01a2eaf1e2
alanyuan97 <alanyuan97@gmail.com>
2018-11-26 17:53:32 +0000

    fixed testcase with subu

--- src/testcase/subu.s
+++ src/testcase/subu.s
@@ -1,5 +1,5 @@
 #subu,254,,,simple unsigned sub
 li $3,0xFFFFFFFF
 addu  $2,$0,1
-subu  $2,$3,1
+subu  $2,$3,$2
 jr    $0
--- src/testcase/subu_cc.s
+++
@@ -1,5 +0,0 @@
-#subu,246,,,math exception -10
-li $3,0xFFFFFFFF
-addu  $2,$0,1
-subu  $2,$3,1
-jr    $0
