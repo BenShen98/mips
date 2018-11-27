34d570f2a42fd1fce655a807f35df490e9bcaf35
Ben Shen <shenleyang007@gmail.com>
2018-11-27 19:56:21 +0000

    fix case name

--- src/testcase/multu_cc.s
+++ src/testcase/multu_cc.s
@@ -1,4 +1,4 @@
-#multu_cc,254,,,should return 254 with no errors
+#multu,254,,,should return 254 with no errors
 lui $3,65535
 ori $3,$3,65535
 multu $3,$3
--- src/testcase/ori_cc.s
+++ src/testcase/ori_cc.s
@@ -1,4 +1,4 @@
-#ori_cc,3,,,ori should not sign extend
+#ori,3,,,ori should not sign extend
 addiu $4,$0,0x7FFF
 ori  $2,$4,0xFFFF
 srl $2,$2,14
